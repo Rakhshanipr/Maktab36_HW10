64a2dcd335e82af3685bb8a38f6860b20cd0bc1d
Rakhshanipr <you@example.com>
2020-08-04 17:01:00 +0430

    + model.Player.java + fragment and layouts : TicTacToeFragment.java , FourInRowFragment.java , GameNameButtonsFragment.java + image : Circle.png,multile.png create desgin of fragment_tic_tac_toe.xml
    set Transaction of MainActivity.java

---
+++ app/src/main/java/com/example/maktab36_hw10/Controller/activity/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.maktab36_hw10.Controller.activity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.maktab36_hw10.Controller.fragment.GameNameButtonsFragment;
+import com.example.maktab36_hw10.Controller.fragment.four_in_row.FourInRowFragment;
+import com.example.maktab36_hw10.Controller.fragment.tic_tac_toe.TicTacToeFragment;
+import com.example.maktab36_hw10.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager();
+        initSitting();
+    }
+
+    private void fragmentManager() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_game_main_container);
+        if (fragment == null) {
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_game_main_container, new TicTacToeFragment())
+                    .add(R.id.fragment_game_main_container, new FourInRowFragment())
+                    .add(R.id.fragment_game_button_container, new GameNameButtonsFragment())
+                    .commit();
+        }
+    }
+
+    private void initSitting() {
+        getSupportActionBar().hide();
+    }
+
+}
---
+++ app/src/main/java/com/example/maktab36_hw10/model/Player.java
@@ -0,0 +1,104 @@
+package com.example.maktab36_hw10.model;
+
+public class Player {
+    private String mName;
+    private int mPoint;
+    private boolean mActive;
+    private static Player sPlayer1;
+    private static Player sPlayer2;
+
+    private Player(String name, int point) {
+        mName = name;
+        mPoint = point;
+        mActive = false;
+    }
+
+    public void run() {
+        sPlayer1 = new Player("Player 1", 0);
+        sPlayer2 = new Player("Player 2", 0);
+    }
+
+    public static Player getPlayer1() {
+        return sPlayer1;
+    }
+
+    public static Player getPlayer2() {
+        return sPlayer2;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public int getPoint() {
+        return mPoint;
+    }
+
+    public void setPoint(int point) {
+        mPoint = point;
+    }
+
+    public boolean isActive() {
+        return mActive;
+    }
+
+    public void setActive(boolean active) {
+        mActive = active;
+    }
+
+    //    public String getName1() {
+//        return mPlayer1.mName;
+//    }
+//
+//    public void setName1(String name) {
+//        mPlayer1.mName = name;
+//    }
+//
+//
+//    public String getName2() {
+//        return mPlayer2.mName;
+//    }
+//
+//    public void setName2(String name) {
+//        mPlayer2.mName = name;
+//    }
+//
+//
+//    public int getPoint1() {
+//        return mPlayer1.mPoint;
+//    }
+//
+//    public void setPoint1(int point) {
+//        mPlayer1.mPoint = point;
+//    }
+//
+//
+//    public int getPoint2() {
+//        return mPlayer2.mPoint;
+//    }
+//
+//    public void setPoint2(int point) {
+//        mPlayer2.mPoint = point;
+//    }
+//
+//    public boolean isActive1() {
+//        return mPlayer1.mActive;
+//    }
+//
+//    public void setActive1(boolean active) {
+//        mPlayer1.mActive = active;
+//    }
+//
+//    public boolean isActive2() {
+//        return mPlayer2.mActive;
+//    }
+//
+//    public void setActive2(boolean active) {
+//        mPlayer2.mActive = active;
+//    }
+
+}
